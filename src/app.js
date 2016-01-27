0be96feea9a483f5ea946e757134c4332b6e2108
nick <none@none.com>
2016-01-27 17:47:22 -0500

    updated src with demo files to be distributed as a webpack module

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
  * Created by tusharmathur on 5/15/15.
  */
 'use strict'
+
 var _ = require('lodash')
 var utils = require('./lib/Utility')
 var ob
@@ -18,12 +19,40 @@ var defaultOptions = {
 var getContentLength = (res) => parseInt(res.headers['content-length'], 10)
 var rangeHeader = (thread) => ({'range': `bytes=${thread.start}-${thread.end}`})
 var toBuffer = _.partialRight(utils.toBuffer, MAX_BUFFER)
-function download (options) {
+
+function singleThreadDownload(options) {
   const opt = fromJS(options)
+  const writableFile = ob.fsOpen(opt.get('path'), 'w+')
+  const downloadedFile = Rx.Observable.create(observer => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", options.url);
+    xhr.onload = () => {
+      observer.onNext(xhr.response);
+      observer.onCompleted();
+    }
+    xhr.onerror = () => {
+      observer.onError(xhr);
+    }
+    xhr.responseType = "blob";
+    xhr.send();
+  });
+
+  return writableFile
+    .zip(downloadedFile, (fd, buffer) => _.merge(options, {fd: fd, buffer: buffer}))
+    .flatMap(ctx => {
+      return ob.fsWrite(ctx.fd, ctx.buffer, 0, ctx.buffer.length, 0)
+    });
+}
+
+function download (options) {
+  console.log(options)
+  const opt = fromJS(options)
+  console.log(opt)
   var writePositions = fromJS(times(opt.get('threadCount'), 0))
   const writableFile = ob.fsOpen(opt.get('path'), 'w+')
   const downloadSize = ob.requestHead(opt.filter(utils.keyIn(['url', 'strictSSL'])).toJS()).map(getContentLength).filter(_.isFinite)
-  return downloadSize.combineLatest(writableFile, (size, fd) => opt.set('size', size).set('fd', fd))
+
+    return downloadSize.combineLatest(writableFile, (size, fd) => opt.set('size', size).set('fd', fd))
     .map(x => x.set('threads', fromJS(utils.sliceRange(x.get('threadCount'), x.get('size')))))
     .flatMap(x => map(x.get('threads').toJS(), (thread, i) => x.set('headers', fromJS(rangeHeader(thread))).set('threadIndex', i).set('start', thread.start)))
     .tap(x => writePositions = writePositions.set(x.get('threadIndex'), x.get('start')))
@@ -36,29 +65,60 @@ function download (options) {
     .flatMap(x => ob.fsWrite(x.get('fd'), x.get('buffer'), 0, x.get('buffer').length, x.get('writtenPositions').get(x.get('threadIndex')) - x.get('buffer').length), identity)
     .map(x => x.set('metaBuffer', toBuffer(x.filter(utils.keyIn(['fd', 'url', 'writtenPositions', 'path', 'size', 'threads'])).toJS())))
     .flatMap(x => ob.fsWrite(x.get('fd'), x.get('metaBuffer'), 0, x.get('metaBuffer').length, x.get('size')), identity)
-    .last()
+        .last().do((x)=>{console.log("complete")})
     .flatMap(x => ob.fsTruncate(x.get('fd'), x.get('size')), identity)
     .flatMap(x => ob.fsRename(x.get('path'), x.get('path').replace('.mtd', '')), identity)
 }
 
 class Download {
   constructor (options) {
+    console.log('Donwload constructor')
     if (ob === undefined) {
       throw new Error('Need to set ob library!');
     }
     this.options = _.defaults(options, defaultOptions)
-    this.options.path += '.mtd'
+    this.options.path += '.mtd';
   }
 
   static setOb (b) {
-    ob = require(b)
+    console.log('setOb')
+    if(typeof b === "string") {
+      b = b.toLowerCase();
+      switch(b) {
+        case "indexeddb":
+          ob = require("./lib/IndexedDB");
+          break;
+        case "webfs":
+          ob = require("./lib/WebFS");
+          break;
+        default:
+          console.error("Invalid Observable library!");
+      }
+      return;
+    } else if(typeof b === 'function') {
+      ob = b;
+    }
   }
 
   start () {
-    return download(this.options).toPromise()
+    console.log('start')
+    let d = download(this.options);
+    return d.toPromise();
+  }
+
+  startSingle() {
+    console.log('start')
+    this.options.path = this.options.path.slice(0,-4); //remove .mtd
+    let d = singleThreadDownload(this.options);
+    return d.toPromise();
+  }
+
+  fsRead(path, cb) {
+    ob.fsRead(path, cb);
   }
 
   stop () {
   }
 }
+
 module.exports = Download
--- src/lib/IndexedDB.js
+++ src/lib/IndexedDB.js
@@ -1,3 +1,7 @@
+//import request from 'request'
+import Rx from 'rx'
+import _ from 'lodash'
+
 const DB_NAME = 'downloader';
 const DB_VERSION = 1;
 const DB_STORE_NAME = 'publications';
@@ -21,74 +25,146 @@ let getObjectStore = (store_name, mode) => {
  * @param {string} fileName
  * @param {Blob=} blob
  */
-let addPublication = (fileName, blob) => {
-  console.log('addPublication arguments:', arguments);
-  let obj = { fileName: fileName };
+let addPublication = (db, fileName, blob, cb) => {
+  //console.log('addPublication arguments:', arguments);
+  let obj = { fileName: fileName, id: 0 };
   if (typeof blob != 'undefined')
     obj.blob = blob;
 
   let store = getObjectStore(DB_STORE_NAME, 'readwrite');
   let req;
   try {
-    req = store.add(obj);
+    req = store.put(obj);
   } catch (e) {
     if (e.name == 'DataCloneError')
       console.error("This engine doesn't know how to clone a Blob, use Firefox");
-    throw e;
+    return cb(e);
   }
   req.onsuccess = (evt) => {
     console.log('Insertion in DB successful');
+    cb(false, evt);
   };
   req.onerror = () => {
     console.error('addPublication error', this.error);
+    cb(err);
   };
 }
+/*
+let requestBody = (params) => {
+  var responseHeaders
+  return Rx.Observable.create((observer) => {
+    request(params)
+      .on('data', buffer => observer.onNext({buffer, headers: responseHeaders}))
+      .on('response', x => responseHeaders = x.headers)
+      .on('complete', x => observer.onCompleted(x))
+      .on('error', x => observer.onError(x))
+  })
+}
+*/
+let requestHeadWrapped = (options, cb) => {
+  let xhr = new XMLHttpRequest();
+  let res = {headers: {}};
 
-let requestBody = () => {
+  xhr.open('HEAD', options.url);
+  xhr.onload = () => {
+    res.headers['content-length'] = xhr.getResponseHeader('Content-Length');
+    console.log('request head cb', res)
+    cb(false, res);
+  }
+  xhr.onerror = () => {
+  }
+  xhr.send();
 }
 
-let requestHead = () => {
-}
-
-let fsOpen = () => {
+let fsOpen = (path, flags, ...rest) => {
+  let cb = rest[rest.length-1];
   console.log('openDb ...');
   let req = indexedDB.open(DB_NAME, DB_VERSION);
   req.onsuccess = (evt) => {
-    db = this.result;
+    db = evt.target.result;
     console.log('openDb DONE', db);
+    cb(false, {db: db, fileName: path});
   };
   req.onerror = (evt) => {
     console.error('openDb:', evt.target.errorCode);
+    cb(evt);
   };
   req.onupgradeneeded = (evt) => {
     console.log('openDb.onupgradeneeded');
     var store = evt.currentTarget.result.createObjectStore(
-      DB_STORE_NAME, { keyPath: 'id', autoIncrement : true });
+      DB_STORE_NAME, { keyPath: 'fileName', autoIncrement : false });
 
-    store.createIndex('fileName', 'fileName', { unique: false });
+    store.createIndex('blob', 'blob', { unique: false });
   };
 }
 
-let fsWrite = () => {
+let fsWrite = (ctx, buffer, offset, length, position, cb) => {
+  let {db, fileName} = ctx;
   console.log('add ...');
 
   // put sth here
-  addPublication(fileName, blob);
+  addPublication(db, fileName, buffer, (err, evt) => {
+    cb(err, evt, ctx);
+  });
 }
 
-let fsTruncate = () => {
+let fsRead = (path, cb) => {
+  if(db) doRead();
+  else {
+    fsOpen(path, "w+", (e, fd) => {
+      if(e) return cb(e);
+      else doRead();
+    });
+  }
+
+  function doRead() {
+    let store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    let req = store.get(path);
+    console.log("getting " + path);
+    req.onsuccess = (evt) => {
+      cb(false, evt.target.result, evt);
+    }
+
+    req.onerror = (evt) => {
+      cb(evt);
+    }
+  }
+
 }
 
-let fsRename = () => {
+// let fsTruncate = () => {
+// }
+
+let fsRename = (oldName, newName, cb) => {
+  let store = getObjectStore(DB_STORE_NAME, 'readwrite');
+  let request = store.get(oldName);
+  request.onerror = (evt) => {
+    console.error('fsRename:', evt.target.errorCode);
+    cb(evt);
+  };
+  request.onsuccess = (evt) => {
+    let data = request.result;
+
+    data.fileName = newName;
+    let requestUpdate = objectStore.put(data);
+    requestUpdate.onerror = (evt) => {
+      console.error('requestUpdate:', evt.target.errorCode);
+      cb(evt);
+    };
+    requestUpdate.onsuccess = (evt) => {
+      console.log('fsRename DONE');
+    };
+  };
 }
 
 supported();
 
 module.exports = {
-  // requestBody,
-  // requestHead,
-  fsOpen,
-  // fsWrite,
+  //requestBody,
+  requestHead: Rx.Observable.fromNodeCallback(requestHeadWrapped, null, _.identity),
+  fsOpen: Rx.Observable.fromNodeCallback(fsOpen),
+  fsWrite: Rx.Observable.fromNodeCallback(fsWrite),
   // fsTruncate,
-  // fsRename
+  fsRename
 }
+module.exports.fsRead = fsRead;
